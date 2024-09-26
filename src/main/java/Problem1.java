f6f2c9dd57292c3a22230ca3d24dc523ff7a776a
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2024-09-26 14:34:37 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,9 +1,32 @@
-// Problem1.java
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        // TODO: Implement the concatenate method
-        // This method should concatenate l2 to l1 and return l1
-        // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        if (l1 == null) {
+            return copyList(l2);
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode current = l1;
+        while (current.next != null) {
+            current = current.next;
+        }
+        ListNode l2Copy = copyList(l2);
+        current.next = l2Copy;
+        return l1;
+    }
+
+    private static ListNode copyList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode newHead = new ListNode(head.val);
+        ListNode current = newHead;
+        ListNode originalCurrent = head.next;
+        while (originalCurrent != null) {
+            current.next = new ListNode(originalCurrent.val);
+            current = current.next;
+            originalCurrent = originalCurrent.next;
+        }
+        return newHead;
     }
 }
